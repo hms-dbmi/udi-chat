982c53f9d20a351ce183525e2a8e4358daae6279
dev-lan <devinscottlange@gmail.com>
2025-02-19 15:45:54 -0700

    rearrange folder structure

--- quasar.config.js
+++ quasar.config.js
@@ -188,7 +188,7 @@ module.exports = configure(function (/* ctx */) {
       builder: {
         // https://www.electron.build/configuration/configuration
 
-        appId: 'scout',
+        appId: 'yac',
       },
     },
 
