f3c4a52637f5c8d930eea99cfc119aea4bae060d
dev-lan <devinscottlange@gmail.com>
2025-07-09 16:58:40 -0600

    add basic layout for dashboard

---
+++ src/components/VizDashboard.vue
@@ -0,0 +1,9 @@
+<script setup lang="ts">
+import { ref, computed, watch, onMounted } from 'vue';
+</script>
+
+<template>
+  <div>Placeholder for pinned visualizations</div>
+</template>
+
+<style scoped lang="scss"></style>
--- src/pages/IndexPage.vue
+++ src/pages/IndexPage.vue
@@ -1,12 +1,24 @@
 <template>
-  <q-page class="column items-center justify-evenly">
-    <chat-interface></chat-interface>
+  <q-page class="row">
+    <q-splitter v-model="splitterModel">
+      <template v-slot:before>
+        <div class="full-height column items-center justify-evenly">
+          <chat-interface></chat-interface>
+        </div>
+      </template>
+      <template v-slot:after>
+        <viz-dashboard></viz-dashboard>
+      </template>
+    </q-splitter>
   </q-page>
 </template>
 
 <script setup lang="ts">
-// import { ref } from 'vue';
+import { ref } from 'vue';
 import ChatInterface from 'components/ChatInterface.vue';
+import VizDashboard from 'components/VizDashboard.vue';
+
+const splitterModel = ref(50);
 
 defineOptions({
   name: 'IndexPage',
