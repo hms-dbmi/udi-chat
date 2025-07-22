835e0b828f1be89fc34c20f0442e1049e09c4ff3
dev-lan <devinscottlange@gmail.com>
2025-07-21 20:51:05 -0600

    hide placeholder text

--- src/components/FilterToolbar.vue
+++ src/components/FilterToolbar.vue
@@ -3,7 +3,7 @@ import { ref, computed, watch, onMounted } from 'vue';
 </script>
 
 <template>
-  <div>Placeholder for Filer toolbar thingy</div>
+  <!-- <div>Placeholder for Filer toolbar thingy</div> -->
 </template>
 
 <style scoped lang="scss"></style>
